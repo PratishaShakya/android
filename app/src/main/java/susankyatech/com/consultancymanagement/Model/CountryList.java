515bbd1b6a0417873297eedc23b25e94ba74efd0
Attikz <pratikzhudson@gmail.com>
2018-11-01 18:10:42 +0545

    - Country list added!

---
+++ app/src/main/java/susankyatech/com/consultancymanagement/Model/CountryList.java
@@ -0,0 +1,30 @@
+package susankyatech.com.consultancymanagement.Model;
+
+public class CountryList {
+
+    private String countyList [] = {
+            "India",
+            "Bangladesh",
+            "Australia",
+            "Poland",
+            "Germany",
+            "USA",
+            "Canada",
+            "England",
+            "Finland",
+            "Japan",
+            "China",
+            "South Korea",
+            "Philippines",
+            "Thailand",
+            "New Zealand",
+            "Italy",
+            "UK",
+            "France",
+            "Netherlands",
+            "Russia",
+            "Switzerland",
+            "Belgium",
+            "Spain"
+    };
+}
